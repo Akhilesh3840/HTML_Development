5ddb33139f769d57acb0de07f8fe6ccf1bc84a78
Akhilesh3840 <akhalesh.pal1@gmail.com>
2025-02-22 20:52:26 +0530

    Update scripts.js
    Added script

--- shared/js/scripts.js
+++ shared/js/scripts.js
@@ -2,3 +2,16 @@ $(function(){
 	$('#header').load('shared/header.html');
 	$('#footer').load('shared/footer.html');	
 });
+
+
+$(document).ready(function() {
+	$(".search").click(function() {
+	   $(".search-box").toggle();
+	   $("input[type='text']").focus();
+	 });
+
+	 $(".toggle-bar").click(function(){
+		$(".header-menu").toggleClass("open-menu");
+	});
+
+ });
